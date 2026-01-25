4aecc291edccfa906117b642a974171b5fd842ef
tomo <tomoy37@gmail.com>
2026-01-25 21:58:27 +0900

    Deploy fixed async captureImage

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/PlantARApp-Web/',
+  base: './',
   server: {
     host: '0.0.0.0',
     port: 5173
